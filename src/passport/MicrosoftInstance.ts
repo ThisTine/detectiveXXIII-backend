b402bf1f2c3fa26b3d627f5254088cf65ec2d351
I_AM_TINE <sutichok46@gmail.com>
2022-07-08 15:59:20 +0700

    fix profile pic

--- src/passport/MicrosoftInstance.ts
+++ src/passport/MicrosoftInstance.ts
@@ -4,9 +4,14 @@ import OAuth2Strategy from 'passport-oauth2'
 import axios from 'axios'
 const verfy:(prisma:PrismaClient)=>OAuth2Strategy.VerifyFunction = (prisma)=> 
 async (accessToken:string, refreshToken:string, profile:any, done:OAuth2Strategy.VerifyCallback)=>{
-    const pf = await axios.get(`https://graph.microsoft.com/beta/me/photos/432x432/$value`,{headers:{Authorization:`Bearer ${accessToken}`,"Content-Type":"image/jpg"},responseType:"arraybuffer"})
+        let pf:any;
     try{
-        const {data:{onPremisesSamAccountName}} = await axios.get(`https://graph.microsoft.com/v1.0/users/${profile.id}?$select=onPremisesSamAccountName`,{headers:{Authorization:`Bearer ${accessToken}`,"Content-Type":"application/json"}})
+        pf = await axios.get(`https://graph.microsoft.com/beta/me/photos/432x432/$value`,{headers:{Authorization:`Bearer ${accessToken}`,"Content-Type":"image/jpg"},responseType:"arraybuffer"})
+    }catch(err){
+        pf = null
+    }
+    try{
+       const {data:{onPremisesSamAccountName}} = await axios.get(`https://graph.microsoft.com/v1.0/users/${profile.id}?$select=onPremisesSamAccountName`,{headers:{Authorization:`Bearer ${accessToken}`,"Content-Type":"application/json"}})
         const { _json} = profile
         if(!onPremisesSamAccountName || !(((onPremisesSamAccountName as string).startsWith("64") || (onPremisesSamAccountName as string).startsWith("65")) && _json.officeLocation === "Computer Science") || !onPremisesSamAccountName || !_json ){
            throw new Error("You don't have permission to enter !")
