7d8fe9bc555034409a69fd86073221f1a08b3856
BlueBox <pongsapukanuvech@gmail.com>
2022-07-14 20:01:34 +0700

    CreateEvenGroup02

--- src/router/admin/routes/createEventGroup.ts
+++ src/router/admin/routes/createEventGroup.ts
@@ -3,26 +3,34 @@ import { Request, Response } from "express";
 import { join } from "path";
 import { eventGroup } from "./randomEventGroup";
 
-interface createEventGroupRequest {
-    hints: {id:string}[]
+interface createEventGroupRequest
+{
+    hints: { id: string }[]
 }
 
 // Gun ( BlueBox ) && Boss ( Sorrawit )
 // สร้าง event_group จาก event_hints id ที่เอามาใส่
 
-const createEventGroup = async (req:Request<any,any,createEventGroupRequest>,res:Response<eventGroup>)=>{
+const createEventGroup = async ( req: Request<any, any, createEventGroupRequest>, res: Response<eventGroup> ) =>
+{
     try
     {
         const group = await req.prisma.event_Group.create( {
-            include: { eventOnhints: { select: { hint: { select: {text: true,id: true} } } } },
+            include: {
+                eventOnhints: { select: { hint: { select: { text: true, id: true } } } },
+                users: true
+            },
+
             data: {
-                eventOnhints: { createMany: { data: [ ...req.body.hints.map( ( item ) => ( { hint_id: item.id } ) ) ] } }      
+                eventOnhints: { createMany: { data: [ ...req.body.hints.map( ( item ) => ( { hint_id: item.id } ) ) ] } }
             }
         } )
+        const rlakesponse = { users: group.users, id: group.id, hints: group.eventOnhints.map( ( item ) => ( { location: item.hint.text, id: item.hint.id } ) ) }
         console.log( group );
-        // res.send(group)
-      } catch (err:any) {
-        res.send(err)
+        res.send( rlakesponse )
+    } catch ( err: any )
+    {
+        res.send( err )
     }
 }
 
