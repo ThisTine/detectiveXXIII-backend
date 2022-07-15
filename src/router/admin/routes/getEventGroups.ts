a4fff0ec9c43be8e5bc256e5ca643acb04fd3786
BlueBox <pongsapukanuvech@gmail.com>
2022-07-15 20:53:43 +0700

    randomEventGroup01 and edit query getEventGroups

--- src/router/admin/routes/getEventGroups.ts
+++ src/router/admin/routes/getEventGroups.ts
@@ -5,7 +5,7 @@ import { eventGroups } from "./randomEventGroup";
 // Gun ( BlueBox ) && Boss ( Sorrawit )
 const getEventGroups = async (req: Request, res: Response<eventGroups>) => {
   try {
-    const { prisma } = req
+    const { prisma } = req;
     const query = await prisma.event_Group.findMany({
       select: {
         id: true,
@@ -14,30 +14,37 @@ const getEventGroups = async (req: Request, res: Response<eventGroups>) => {
             hint: {
               select: {
                 id: true,
-                text: true
-              }
-            }
-          }
+                text: true,
+              },
+            },
+          },
         },
-        users: true
-      }
-    })
+        users: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            year: true,
+          },
+        },
+      },
+    });
 
     const resQuery = query.map((item) => ({
       id: item.id,
       hints: item.eventOnhints.map((hintItem) => ({
         location: hintItem.hint.text,
-        id: hintItem.hint.id
+        id: hintItem.hint.id,
       })),
-      users: item.users
-    }))
+      users: item.users,
+    }));
 
     res.send({
-      groups: resQuery
-    })
+      groups: resQuery,
+    });
   } catch (err: any) {
-    res.send(err)
+    res.send(err);
   }
-}
+};
 
-export default getEventGroups
+export default getEventGroups;
