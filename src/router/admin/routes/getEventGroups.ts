475f396e336a107fc3002eddd6676206e2ff229c
sora-k46 <sorrawit456789@gmail.com>
2022-07-15 12:53:25 +0700

    Implement getEventsGroups

--- src/router/admin/routes/getEventGroups.ts
+++ src/router/admin/routes/getEventGroups.ts
@@ -3,8 +3,41 @@ import { eventGroups } from "./randomEventGroup";
 
 // get eventgroup ทั้งหมด
 // Gun ( BlueBox ) && Boss ( Sorrawit )
-const getEventGroups = (req:Request,res:Response<eventGroups>)=>{
+const getEventGroups = async (req: Request, res: Response<eventGroups>) => {
+  try {
+    const { prisma } = req
+    const query = await prisma.event_Group.findMany({
+      select: {
+        id: true,
+        eventOnhints: {
+          select: {
+            hint: {
+              select: {
+                id: true,
+                text: true
+              }
+            }
+          }
+        },
+        users: true
+      }
+    })
 
+    const resQuery = query.map((item) => ({
+      id: item.id,
+      hints: item.eventOnhints.map((hintItem) => ({
+        location: hintItem.hint.text,
+        id: hintItem.hint.id
+      })),
+      users: item.users
+    }))
+
+    res.send({
+      groups: resQuery
+    })
+  } catch (err: any) {
+    res.send(err)
+  }
 }
 
 export default getEventGroups
