34a975a4250f87d7157a793e845f67325cf2541b
sora-k46 <sorrawit456789@gmail.com>
2022-07-15 12:59:34 +0700

    Format code

--- src/router/admin/routes/randomEventGroup.ts
+++ src/router/admin/routes/randomEventGroup.ts
@@ -1,15 +1,27 @@
 import { Request, Response } from "express";
 
-export type eventGroup = {id:string,hints:{location:string,id:string}[],users:{id:string,name:string,email:string}[]}
+export type eventGroup = {
+    id: string,
+    hints: {
+        location: string,
+        id: string
+    }[],
+    users: {
+        id: string,
+        name: string,
+        email: string
+    }[]
+}
 
-export interface eventGroups { 
-    groups: eventGroup[] }
+export interface eventGroups {
+    groups: eventGroup[]
+}
 
 //Gun ( BlueBox ) && Boss ( Sorrawit )
 
 // random user ทั้งหมด เข้าไป event group อย่างละเท่า ๆ กัน
 
-const randomEventGroup = (req:Request,res:Response<eventGroups>)=>{
+const randomEventGroup = (req: Request, res: Response<eventGroups>) => {
 
 }
 
