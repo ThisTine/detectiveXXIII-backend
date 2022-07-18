27a5054e9eed4bbb077377f4ca0ba04899c10dec
I_AM_TINE <sutichok46@gmail.com>
2022-07-18 21:22:17 +0700

    delete user

--- src/router/admin/routes/deleteUser.ts
+++ src/router/admin/routes/deleteUser.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, Response } from "express"
 
 interface DeletedUser {
     id: string
@@ -8,8 +8,25 @@ interface DeletedUser {
 
 // ลบ user ตาม id
 
-const deleteUser = (req:Request,res:Response<DeletedUser>)=>{
-
+const deleteUser = async (req: Request<any, any, DeletedUser>, res: Response<DeletedUser>) => {
+    try {
+        const { prisma } = req
+        const fuser = await prisma.user.findFirst({ where: { id: req.body.id }, select: { room: true, id: true } })
+        if (fuser && fuser.room) {
+            if (fuser.room.user_count === 1) {
+                await prisma.room.delete({ where: { id: fuser.room.id } })
+            } else {
+                await prisma.room.update({ where: { id: fuser.room.id }, data: { user_count: { decrement: -1 } } })
+            }
+        }
+        const user = prisma.user.delete({ where: { id: req.body.id } })
+        const user_Opened_Code = prisma.user_Opened_Code.deleteMany({ where: { user_id: req.body.id } })
+        const hints = prisma.hint.deleteMany({ where: { user_id: req.body.id } })
+        await prisma.$transaction([hints, user_Opened_Code, user])
+        res.send({ id: fuser?.id || "" })
+    } catch (err: any) {
+        res.status(500).send(err.toString())
+    }
 }
 
 export default deleteUser
