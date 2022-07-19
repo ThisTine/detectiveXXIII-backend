f507af7b06d10ed5c0a9b333fece0462c41d5a0f
Neng Sitiporn <sitiporn.wimo@kmutt.ac.th>
2022-07-19 21:36:41 +0700

    getEvent Done

--- src/router/user/routes/getEvent.ts
+++ src/router/user/routes/getEvent.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, Response } from "express"
 
 interface Event {
     location: string
@@ -8,11 +8,39 @@ interface Event {
 
 // query hint จาก event_group โดยเอาใน index ที่ event_hints_count
 
-const getEvent = async(req:Request,res:Response<Event>)=>{
+const getEvent = async (req: Request, res: Response<Event | String>) => {
     try {
-        
-    } catch (error : any) {
-        
+        const { prisma } = req
+        if (!req.gameConfig.isEventReady) {
+            return res.status(400).send("Event is not ready !!")
+        }
+        const eventHints = await prisma.user.findFirst({
+            where: {
+                id: req.user?.id,
+            },
+            select: {
+                event_hints_count: true,
+                event_group: {
+                    select: {
+                        eventOnhints: {
+                            select: {
+                                hint: true,
+                            },
+                        },
+                    },
+                },
+            },
+        })
+        if (!eventHints?.event_group) {
+            return res.status(400).send("Group not found")
+        }
+        const hint = eventHints?.event_group?.eventOnhints[eventHints.event_hints_count]
+        if (!hint) {
+            return res.status(400).send("non hint")
+        }
+        return res.send({ location: hint.hint.text })
+    } catch (error: any) {
+        res.send(error)
     }
 }
 
