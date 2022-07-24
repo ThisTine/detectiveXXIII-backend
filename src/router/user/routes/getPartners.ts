10cd7851cc1e36448a01518f0160295c97eb034a
I_AM_TINE <sutichok46@gmail.com>
2022-07-24 16:50:10 +0700

    get partner

--- src/router/user/routes/getPartners.ts
+++ src/router/user/routes/getPartners.ts
@@ -1,17 +1,37 @@
-import { Request, Response } from "express";
+import { Request, Response } from "express"
 
 interface Partner {
-    partners: {userId:string,name:string,img:{type:"Buffer",data:number[]} | null}[]
+    partners: { userId: string; name: string; img: Buffer | null }[]
 }
 
 // Tine ( Thistine ) & Thun ( Bsthun )
 
-// verify ว่า user คนนั้นได้เปิดสายรหัสไปหรือยัง จาก user_opened_codes 
+// verify ว่า user คนนั้นได้เปิดสายรหัสไปหรือยัง จาก user_opened_codes
 // ถ้าเปิดโค้ดที่ id ตรงกับ id ของ partner แล้วก็ให้ return partner คนนั้นตาม response type
 // หรือถ้าโค้ดเราโดนเปิดโดย user id ที่เป็น partner เราแล้ว (เช็คจาก user_opened_code) ก็ให้ return partners เหมือนกัน
 
-const getPartners = (req:Request,res:Response<Partner>)=>{
-
+const getPartners = async (req: Request, res: Response<Partner | String>) => {
+    try {
+        const { prisma } = req
+        const partners = await prisma.user.findFirst({
+            where: { id: req.user?.id },
+            select: { room: { select: { users: { select: { id: true } }, user_count: true } } },
+        })
+        if (!partners || !partners.room || partners.room.user_count <= 1) {
+            throw new Error("Partner not found")
+        }
+        const code_ids = partners.room.users.map((item) => ({ code_id: item.id }))
+        const user_opened_code = await prisma.user_Opened_Code.findMany({
+            where: { user_id: req.user?.id, OR: [...code_ids] },
+            select: { user: true },
+        })
+        if (user_opened_code.length === 0) {
+            res.status(400).json("Game is not complete yet")
+        }
+        res.send({ partners: user_opened_code.map((item) => ({ img: item.user.img, name: item.user.name, userId: item.user.id })) })
+    } catch (err) {
+        res.status(500).send("Internal server Error")
+    }
 }
 
 export default getPartners
