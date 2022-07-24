1c1e6a3d80173a7144a4149e9e77259f18573822
Neng Sitiporn <sitiporn.wimo@kmutt.ac.th>
2022-07-24 14:54:11 +0700

    Gethints done , openhints done

--- src/router/user/routes/gethints.ts
+++ src/router/user/routes/gethints.ts
@@ -1,17 +1,44 @@
-import { Request, Response } from "express";
+import { Request, Response } from "express"
 
 export interface Hint {
-    hints: {userId:1|2,hint:string,isShow:boolean}[]
+    hints: { userId: 1 | 2; hint: string; isShow: boolean }[]
 }
 
 // Neng ( stthi )
 
-// get hints จาก Room ที่เชื่อมกับ user โดย select ถึงแค่จำนวน opened_hints 
+// get hints จาก Room ที่เชื่อมกับ user โดย select ถึงแค่จำนวน opened_hints
 // ที่เหลือเค้าอยากให้สร้าง hint ปลอมให้หน่อย (อันที่ isShow เป็น false)
 // userId ที่มีเค้าอยากให้เป็น fake userid หรือก็คือจะมีแค่ 1 หรือ 2 เท่านั้น
-
-const getHint = (req:Request,res:Response<Hint>)=>{
-
+export const getAllhints: (req: Request) => Promise<Hint> = async (req) => {
+    const { prisma } = req
+    const opened_hints = await prisma.user.findFirst({
+        where: { id: req.user?.id },
+        select: { opened_hints: true, room: { select: { users: { select: { id: true, hints: true } } } } },
+    })
+    const users = opened_hints?.room?.users.filter((e) => e.id !== (req.user?.id || ""))
+    let response: { userId: 1 | 2; hint: string; isShow: boolean }[] = []
+    users?.forEach((item, index) => {
+        for (let i = 0; i < (opened_hints?.opened_hints || 0); i++) {
+            let e = item.hints[i]
+            response.push({ userId: (index + 1) as any, hint: e.text, isShow: true })
+        }
+    })
+    const length = users?.length === 1 ? response.length : response.length / 2
+    for (let i = length; i < 10; i++) {
+        response.push({ userId: 1 as any, hint: "ฮั่นแน่ !!!", isShow: false })
+        if ((users?.length || 0) > 1) {
+            response.push({ userId: 2 as any, hint: "ฮั่นแน่ !!!", isShow: false })
+        }
+    }
+    return { hints: response }
+}
+const getHint = async (req: Request, res: Response<Hint>) => {
+    try {
+        const response = await getAllhints(req)
+        res.send(response)
+    } catch (error: any) {
+        res.status(500).send(error)
+    }
 }
 
 export default getHint
--- src/router/user/routes/openHint.ts
+++ src/router/user/routes/openHint.ts
@@ -1,5 +1,5 @@
-import { Request, Response } from "express";
-import { Hint } from "./gethints";
+import { Request, Response } from "express"
+import { getAllhints, Hint } from "./gethints"
 
 // Neng ( stthi )
 
@@ -7,9 +7,19 @@ import { Hint } from "./gethints";
 // เช็คว่า user เปิด hint หมดหรือยัง (เช็คจำนวน opened_hints ว่าเท่ากับ 10 มั้ย)
 // เปิด hint ใหม่ (เพิ่ม opened_hints +1 แล้วลด lifes -3 อย่าลืมเช็คด้วยว่าเปิด hint หมดหรือยัง)
 
-
-const openHint = (req:Request,res:Response<Hint>)=>{
-
+const openHint = async (req: Request, res: Response<Hint | String>) => {
+    try {
+        const { prisma } = req
+        const life = await prisma.user.findFirst({ select: { lifes: true }, where: { id: req.user?.id } })
+        if ((life?.lifes || 0) < 3) {
+            res.status(400).send("insufficient lifes")
+        }
+        await prisma.user.update({ where: { id: req.user?.id || "" }, data: { opened_hints: { increment: 1 } } })
+        const getResponse = await getAllhints(req)
+        res.send(getResponse)
+    } catch (error: any) {
+        res.send(error)
+    }
 }
 
 export default openHint
