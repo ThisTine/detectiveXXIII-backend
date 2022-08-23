891fc056052611cb070de034e61f75e4eaae57ff
I_AM_TINE <sutichok46@gmail.com>
2022-08-23 16:56:19 +0700

    fix playable user

--- src/router/admin/routes/randomParing.ts
+++ src/router/admin/routes/randomParing.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from "express";
 const randomParing = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { prisma } = req
-        const users_first_year = await prisma.user.findMany({ where: { room_id: null } })
+        const users_first_year = await (await prisma.user.findMany({ where: { room_id: null, year: 1, isPlayable: true }, include: { hints: true } })).filter((item) => item.hints.length === 10)
         const room_with_no_partner = (await prisma.room.findMany({ select: { _count: { select: { users: true } }, id: true } })).filter((item) => item._count.users === 1)
         let ind = 0;
         // paring with 1st year
@@ -12,7 +12,7 @@ const randomParing = async (req: Request, res: Response, next: NextFunction) => 
         await prisma.$transaction(connect_first_year_to_room)
 
         // paring 3 users
-        const users_with_no_pair = (await prisma.user.findMany({ where: { year: 2 }, select: { room: { include: { _count: { select: { users: true } } } }, room_id: true, id: true } }))
+        const users_with_no_pair = (await prisma.user.findMany({ where: { year: 2 }, select: { room: { include: { _count: { select: { users: true } } } }, room_id: true, id: true, hints: true } })).filter(item => item.hints.length === 10)
             .filter(item => (item.room?._count.users || 0) === 1)
 
         const room_with_pair = (await prisma.room.findMany({ select: { _count: { select: { users: true } }, id: true } })).filter(item => item._count.users === 2)
